0f63ae02e7c92abd3e8037c63afb094d296da9a9
Igor Timarac <igor.timarac@codaxy.com>
2020-01-10 14:38:09 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-for i in .bash_profile .bashrc .dircolors .git .gitignore .vim .vimrc ; do
+#!/bin/zsh
+
+for i in .zshrc .vim .vimrc ; do
 	cp -rf $i ~/
 done
+
+cd ~/
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+nvm install 10
+nvm use 10
+npm install -g pure-prompt
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh-syntax-highlighting
 
